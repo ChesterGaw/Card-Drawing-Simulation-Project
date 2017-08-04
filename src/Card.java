e9a653e69759f51fb481993561c64a283c477775
ChesterGaw <gawalchester@gmail.com>
2017-08-04 12:00:41 +0800

    Card/Main Initial Commit

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+
+public class Card {
+	private int suit;
+	private int count;
+	
+	public Card(int suit, int count) {
+		suit = this.suit;
+		count = this.count;
+	}
+
+	public int getSuit() {
+		return suit;
+	}
+
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+	
+}
